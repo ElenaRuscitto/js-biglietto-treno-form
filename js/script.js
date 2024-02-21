2a96941578364c064fb0f44d814bba7176fe3f07
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-21 16:19:16 +0100

    [add] start js

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let inputNs;
+let inputKm;
+let utenteStandard;
+let utenteJung;
+let utenteSenior;
+const costoKm = 0.21;
+
+console.log (nameSurname, kmPercorrere, utenteStandard, utenteJung, utenteSenior, costoKm);
+
+
+// const nameSurname = document.querySelector ('nameSurname');
+// const kmPercorrere = document.querySelector ('kmPercorrere');
+// const utenteStandard = document.querySelector ('utenteStandard');
+// const utenteJung = document.querySelector ('utenteJung');
+// const utenteSenior = document.querySelector ('utenteSenior');
+
+
+
+// const btn1 = document.querySelector ('btn1');
+// const btn2 = document.querySelector ('btn2');
+
+// const input = document.querySelector ('input');
+
+// btn1.addEventListener('click',function(){
+//   // leggo il contenuto dell'input
+//   console.log(inputNs.value);
+//   console.log(inputKm.value);
+
+// });
+
+
+// const kmPercorrere = parseFloat (prompt ('Quanti km dei percorrere?'));
+
+// const anni = parseInt (prompt ('Quanti anni hai?'));
+
+
+// const costoKm = 0.21;
+
+// let price = inputKm * costoKm;
+// console.log (inputKm, anni, price);
+
+// if (anni <= 17) {
+//   let discount = price * 20 / 100;
+//   price = price - discount;
+// } else if (anni >= 66) {
+//   let discount = price * 40 / 100;
+//   price = price - discount;
+// }
