9965d2c8c27a5f7832c2c61f3402df705ae9b0fe
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-21 17:23:32 +0100

    [add] varie

--- js/script.js
+++ js/script.js
@@ -1,48 +1,25 @@
-let inputNs;
-let inputKm;
-let utenteStandard;
-let utenteJung;
-let utenteSenior;
+
 const costoKm = 0.21;
+const nameSurname = document.querySelector ('.inputNs');
+const kmPercorrere = document.querySelector ('.inputKm');
+const utenteEta = document.querySelector ('.selectEta');
+const btn1 = document.querySelector ('.btn1');
+const btn2 = document.querySelector ('.btn2');
+let price = kmPercorrere * costoKm;
 
-console.log (nameSurname, kmPercorrere, utenteStandard, utenteJung, utenteSenior, costoKm);
+btn1.addEventListener('click',function(){
+ // leggo il contenuto dell'input
+  console.log(nameSurname.value);
+  console.log(kmPercorrere.value);
+  console.log(utenteEta.value);
 
+  if (utenteEta.value === 'utenteJung') {
+    let discount = price * 20 / 100;
+    price = price - discount;
+   } else if (utenteEta.value === 'utenteSenior') {
+    let discount = price * 40 / 100;
+    price = price - discount;
+   }
+});
 
-// const nameSurname = document.querySelector ('nameSurname');
-// const kmPercorrere = document.querySelector ('kmPercorrere');
-// const utenteStandard = document.querySelector ('utenteStandard');
-// const utenteJung = document.querySelector ('utenteJung');
-// const utenteSenior = document.querySelector ('utenteSenior');
-
-
-
-// const btn1 = document.querySelector ('btn1');
-// const btn2 = document.querySelector ('btn2');
-
-// const input = document.querySelector ('input');
-
-// btn1.addEventListener('click',function(){
-//   // leggo il contenuto dell'input
-//   console.log(inputNs.value);
-//   console.log(inputKm.value);
-
-// });
-
-
-// const kmPercorrere = parseFloat (prompt ('Quanti km dei percorrere?'));
-
-// const anni = parseInt (prompt ('Quanti anni hai?'));
-
-
-// const costoKm = 0.21;
-
-// let price = inputKm * costoKm;
-// console.log (inputKm, anni, price);
-
-// if (anni <= 17) {
-//   let discount = price * 20 / 100;
-//   price = price - discount;
-// } else if (anni >= 66) {
-//   let discount = price * 40 / 100;
-//   price = price - discount;
-// }
+console.log(price);
