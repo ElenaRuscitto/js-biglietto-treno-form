2a007453ce54881ac47c03de55ef8b6ed513ef3c
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-22 11:24:22 +0100

    [add] randomizzazione

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const btn1 = document.querySelector ('.btn1');
 const btn2 = document.querySelector ('.btn2');
 const textTicket = document.querySelector ('.text-biglietto');
 const ticket = document.querySelector ('.biglietto');
+let isValidData = true;
 
 
 btn1.addEventListener('click',function(){
@@ -15,18 +16,26 @@ btn1.addEventListener('click',function(){
   console.log(kmPercorrere.value);
   console.log(utenteEta.value);
   let price = kmPercorrere.value * costoKm;
+  let tipoBiglietto = 'Biglietto Standard'
 
   if (utenteEta.value === 'utenteJung') {
     let discount = price * 20 / 100;
     price = price - discount;
+    tipoBiglietto = 'Sconto Minorenne'
    } else if (utenteEta.value === 'utenteSenior') {
     let discount = price * 40 / 100;
     price = price - discount;
+    tipoBiglietto = 'Sconto Senior'
    }
+   //genero elementi random Carrozza e CP
+   const numCazzozza = Math.ceil(Math.random() * 12); //non funziona
+   const codiceCp = Math.floor(Math.random() * (100000 - 900000)) + 900000; //non funziona
 
    document.getElementById ('outputNameSurname').innerHTML = nameSurname.value;
-   document.getElementById ('outputUtenteEta').innerHTML = utenteEta.value;
+   document.getElementById ('outputUtenteEta').innerHTML = tipoBiglietto;
    document.getElementById ('price').innerHTML = price.toFixed(2);
+   document.getElementById ('outNumCazzozza').innerHTML = numCazzozza; //non funziona
+   document.getElementById ('outCodiceCp').innerHTML = codiceCp; //non funziona
 
    textTicket.classList.remove('d-none');
    ticket.classList.remove('d-none');
